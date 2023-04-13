beb063705d2bf11792e47a9e43c111d724cb5a24
Ana Maria Nitu <anamarianitu5@gmail.com>
2023-04-13 14:01:07 +0200

    Combine microservices
    merge posts, likes and comments into a single microservice called post-service

---
+++ backend/worldwanderers/post-service/src/main/java/com/backend/postservice/exceptions/CommentNotFound.java
@@ -0,0 +1,10 @@
+package com.backend.postservice.exceptions;
+
+public class CommentNotFound extends Exception {
+
+    public CommentNotFound(String message)
+    {
+
+        super(message);
+    }
+}
